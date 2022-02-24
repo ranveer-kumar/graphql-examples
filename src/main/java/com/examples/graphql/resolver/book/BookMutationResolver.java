51728565c05483afd6cdb1efe404faf0b6841603
ranveer-kumar <ranveer.solr@gmail.com>
2022-02-24 08:50:36 +0530

    in progress: MetaData with metadata

--- src/main/java/com/examples/graphql/resolver/book/BookMutationResolver.java
+++ src/main/java/com/examples/graphql/resolver/book/BookMutationResolver.java
@@ -7,6 +7,7 @@ import com.examples.graphql.model.Book;
 import com.examples.graphql.model.BookInput;
 import com.examples.graphql.model.Temp;
 import com.examples.graphql.repository.BookRepository;
+import com.examples.graphql.util.CommonUtils;
 import graphql.kickstart.tools.GraphQLMutationResolver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.validation.Valid;
+import java.security.SecureRandom;
 
 @Component
 @Slf4j
@@ -30,24 +32,25 @@ public class BookMutationResolver implements GraphQLMutationResolver {
         Book book = new Book();
 //        log.info(articleInput.getMetaData().toString());
         BeanUtils.copyProperties(bookInput, book);
-        book.setId(bookInput.getId());
-        book.setDomainId(bookInput.getDomainId());
-        book.setTitle(bookInput.getTitle());
+        book.setId((long) new SecureRandom().nextInt(100000));
+        book.getMetaData().setUrl(CommonUtils.urlGenerator(book.getTitle(), book.getId()));
+//        book.setDomainId(bookInput.getDomainId());
+//        book.setTitle(bookInput.getTitle());
 //        book.setMetaData(MetaData.builder()
 //                        .metaTitle(bookInput.getMetaData().getMetaTitle())
 //                        .authorEmail(bookInput.getMetaData().getAuthorEmail())
 //                        .url(bookInput.getMetaData().getUrl())
 //                        .canonicalUrl(bookInput.getMetaData().getCanonicalUrl())
 //                .build());
-        book.setAuthor(Author.builder()
-                        .name(bookInput.getAuthor().getName())
-                        .city(bookInput.getAuthor().getCity())
-                .build());
-        book.setTemp(Temp.builder()
-                        .metaTitle(bookInput.getTemp().getMetaTitle())
-                        .metaDescription(bookInput.getTemp().getMetaDescription())
-                        .url(bookInput.getTemp().getUrl())
-                .build());
+//        book.setAuthor(Author.builder()
+//                        .name(bookInput.getAuthor().getName())
+//                        .city(bookInput.getAuthor().getCity())
+//                .build());
+//        book.setTemp(Temp.builder()
+//                        .metaTitle(bookInput.getTemp().getMetaTitle())
+//                        .metaDescription(bookInput.getTemp().getMetaDescription())
+//                        .url(bookInput.getTemp().getUrl())
+//                .build());
 
         log.info("start saving the article");
         return bookRepository.save(book);
